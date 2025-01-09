04ff0dbdbd171111a934a0e8c6c2b1c6f3ed69fa
manuelpezzi <pezzimanuel@gmail.com>
2025-01-09 14:25:03 +0100

    new function+ user expirience

--- index.js
+++ index.js
@@ -9,13 +9,15 @@
 
 
 function calcolaPrezzoTotale(km, eta) {
+    const prezzoPerKm = 0.21; // Prezzo Per kilometro
+    let prezzoTotale = prezzoPerKm * km; // calcolo prezzo base
 
     // Applicazione degli sconti
-    if (Età < 18) {
+    if (eta < 18) {
         // Sconto del 20% per minorenni
         prezzoFinale -= prezzoFinale * 0.20;
     }
-    else if (Età > 65) {
+    else if (eta > 65) {
         // sconto del 40% per over 65
         prezzoFinale -= prezzoFinale * 0.40;
     }
@@ -23,4 +25,7 @@ function calcolaPrezzoTotale(km, eta) {
 
     console.log("Il prezzo totale del viaggio è: €" + prezzoFinale.toFixed(2));
 }
+// Chiedere all'utente i dati
+let km = parseInt(prompt("Inserisci il numero di chilometri che vuoi percorrere:"));
+let eta = parseInt(prompt("Inserisci la tua età:"));
 
