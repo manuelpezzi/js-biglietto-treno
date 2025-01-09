cb16f534f624996bdde73ec9fbb06cad384d9b41
manuelpezzi <pezzimanuel@gmail.com>
2025-01-09 12:54:33 +0100

    prompt eseguito

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * 1-preparazione variabili del caso con elenco dei vari dati
+ * 2-elenco dei vai if e regole con sconto minorenni del 20% e sconto del 40% per gli over 65
+ * 3-test
+ * 4-output dalla console
+ * 
+ */
+let Prezzo = 0.21
+let Età = parseInt(prompt("scrivi qui la tua età"))
+let kilometro = parseInt(prompt("scrivi quanti kilometri devi percorrere"))
+
